b7dc368ad5531ef0b5e01e7ad135508c72cdbe2d
shubhambattoo <shubham.battoo@hotmail.com>
2020-03-22 11:07:45 +0530

    board design updated

--- src/views/board/Board.js
+++ src/views/board/Board.js
@@ -24,31 +24,40 @@ export const Board = ({ match }) => {
       <div className={styles.ui}>
         <div className={styles.columns}>
           <div className={styles.column}>
-            <header>Main Website</header>
+            <header>
+              Main Website
+              <div className={styles.trash}>
+                <i class="material-icons" style={{ fontSize: '25px' }}>
+                  delete_outline
+                </i>
+              </div>
+            </header>
             <ul>
               <li>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-                incidunt?
-              </li>
-              <li>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate voluptatem veritatis soluta, blanditiis cum ab
-                exercitationem. Unde nostrum incidunt soluta vel tenetur. Ipsam
-                atque non, debitis ipsa consectetur aperiam saepe officiis
-                assumenda est animi aut fugit optio esse eos tenetur laudantium.
-                Nisi quos adipisci numquam maiores deleniti praesentium id
-                repudiandae et assumenda earum harum, architecto provident
-                ducimus perspiciatis rem ipsum beatae sequi error veritatis
-                culpa fugit? Quis quasi alias molestias unde error assumenda
-                magni velit corporis consequatur nostrum architecto minus id
-                quidem facere veritatis, tempore dolore ipsum earum delectus ad,
-                vel repudiandae? Tempore aliquid nesciunt modi laboriosam vero
-                explicabo nostrum.
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque, incidunt?
+                </div>
+                <div className={styles.actions}>
+                  <div style={{ alignSelf: 'center', cursor: 'pointer' }}>
+                    <i class="material-icons" style={{ fontSize: '30px' }}>
+                      list
+                    </i>
+                  </div>
+                  <div className={styles.team}>
+                    <div className={styles.circle}>NN</div>
+                    <div className={styles.circle}>AB</div>
+                  </div>
+                </div>
               </li>
             </ul>
-            <footer>Add a Card</footer>
+            <footer>
+              <button>Add a Card</button>
+            </footer>
           </div>
-          <button id="CreateColumn" className={styles.addButton}>Add column</button>
+          <button id="CreateColumn" className={styles.addButton}>
+            Add column
+          </button>
         </div>
       </div>
     </div>
