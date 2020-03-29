651593fdcc13c0682e5c389a8d5b3f2722a7b0a8
shubhambattoo <shubham.battoo@hotmail.com>
2020-03-29 19:31:32 +0530

    handling team tags

--- src/components/team-tags/Team.js
+++ src/components/team-tags/Team.js
@@ -3,6 +3,9 @@ import styles from './Team.module.css';
 
 export const Team = ({ name }) => {
   const arr = name.split(' ');
-  const abbr = `${arr[0].charAt(0)}${arr[1].charAt(0)}`;
+  let abbr = '';
+  arr.forEach(element => {
+    abbr = element.charAt(0);
+  });
   return <div className={styles.circle}>{abbr}</div>;
 };
