7ef3bd5ac3efb6215584851e04239df0092bed1e
shubhambattoo <shubham.battoo@hotmail.com>
2020-07-19 12:02:14 +0530

    link updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -33,7 +33,7 @@ export const Header = () => {
           {currentUser ? (
             <>
               <li>
-                <NavLink exact activeClassName={styles.activeLink} to="/">
+                <NavLink exact activeClassName={styles.activeLink} to="/app">
                   Home
                 </NavLink>
               </li>
